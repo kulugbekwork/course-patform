e090e442bc3b45bfc37e21178fd277847edaeba3
kulugbekwork <kulugbek.work@gmail.com>
2026-01-09 22:55:43 +0500

    working on env variables

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,14 +1,14 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_KEY;
 
 if (!supabaseUrl) {
   throw new Error('Missing env.VITE_SUPABASE_URL. Please check your .env file.');
 }
 
 if (!supabaseAnonKey) {
-  throw new Error('Missing env.VITE_SUPABASE_ANON_KEY. Please check your .env file.');
+  throw new Error('Missing env.VITE_SUPABASE_KEY. Please check your .env file.');
 }
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
